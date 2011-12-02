20f26adf483166444ae59c5c9e25f55f90372ff6
Douglas Muth <dmuth@dmuth.org>
2011-12-01 23:10:40 -0500

    Added the ability to select a product.

--- form.inc.php
+++ form.inc.php
@@ -18,15 +18,33 @@ function anthrocon_dealers_form() {
 		"#title" => t("Dealer Tables"),
 		);
 
-	$retval["tables"]["download"] = array(
-		"#type" => "submit",
-		"#value" => t("Download This List"),
+	$products = anthrocon_dealers_get_products("Select a Product");
+
+	$nid = variable_get("anthrocon_dealers_nid", "");
+	$retval["tables"]["nid"] = array(
+		"#type" => "select",
+		"#title" => t("Product"),
+		"#options" => $products,
+		"#default_value" => $nid,
 		);
 
-	$retval["tables"]["browser_dump"] = array(
-		"#type" => "checkbox",
-		"#title" => t("Dump to browser instead. (for testing/debugging purposes)"),
+	$retval["tables"]["update_product"] = array(
+		"#type" => "submit",
+		"#value" => t("Change Product"),
 		);
+
+	if ($nid) {
+		$retval["tables"]["download"] = array(
+			"#type" => "submit",
+			"#value" => t("Download This List"),
+			);
+
+		$retval["tables"]["browser_dump"] = array(
+			"#type" => "checkbox",
+			"#title" => t("Dump to browser instead. (for testing/debugging purposes)"),
+			);
+
+	}
 
 	return($retval);
 
@@ -39,13 +57,24 @@ function anthrocon_dealers_form() {
 function anthrocon_dealers_form_submit($form, $form_state) {
 
 	//print "<pre>"; print_r($form_state); print "</pre>"; // Debugging
+	//print "<pre>"; print_r($form_state["values"]); print "</pre>"; // Debugging
 
 	//
 	// What button did we click?
-	// We'll need this if we add more buttons later.
 	//
 	$op = $form_state["values"]["op"];
+	$nid = $form_state["values"]["nid"];
 	$dump = $form_state["values"]["browser_dump"];
+
+	variable_set("anthrocon_dealers_nid", $nid);
+
+	//
+	// If we clicked the "change product" button, stop here.
+	//
+	if ($op == t("Change Product")) {
+		drupal_set_message("Product updated to NID $nid");
+		return(null);
+	}
 
 	//
 	// Retrieve our current tables sold.
